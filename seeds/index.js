e09d6f77533d84c5f068f38b4159dda98ead061e
nukun56788 <107339530+NukunK@users.noreply.github.com>
2025-05-10 17:02:59 +0700

    fetch api image and show in show.ejs

--- seeds/index.js
+++ seeds/index.js
@@ -17,10 +17,13 @@ const seedDB = async() => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20 ) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
-
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi dicta repellat cupiditate mollitia reiciendis veniam accusamus neque nobis ratione hic.',
+            price
         })
         await camp.save();
     }
